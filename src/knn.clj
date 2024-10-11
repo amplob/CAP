eb55ae2dedf6ea8528f67bb8da4f9c4f7201a9b2
Amplob <amplob@gmail.com>
2024-10-11 15:15:57 +0200

    distancia de hamming

--- src/knn.clj
+++ src/knn.clj
@@ -17,9 +17,26 @@
 )
 
 
+(defn comparaIAssigna [a b]
+  (if (= a b)
+    0.0
+    1.0))
+
+
+(defn dh [v w]
+    (/
+        (reduce + (map 
+            (fn [vi wi] (comparaIAssigna vi wi)
+            ) v w)
+        )
+        (count v)
+    )
+)
+
 (println (de [1 0] [1 1]) )
 (println (de [0 0 0] [1 1 1]) )
-
-
 (println (dm [1 0] [1 1]) )
 (println (dm [0 0 0] [1 1 1])  )
+(println  (dh [:a :b] [:b :b]) )
+(println  (dh [:a :a :a] [:b :b :b]) )
+
