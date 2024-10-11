1efa6b40b596e9b3cfdc2fc0bfcd460509412cfd
Amplob <amplob@gmail.com>
2024-10-11 15:02:15 +0200

    de i dm

--- src/knn.clj
+++ src/knn.clj
@@ -1,3 +1,25 @@
 (ns knn)
 
+(defn de [v w]
+    (Math/sqrt
+        (reduce + (map 
+            (fn [vi wi] (Math/pow (- vi wi) 2)) 
+            v w)
+        )
+    )
+)
 
+(defn dm [v w]
+    (reduce + (map 
+        (fn [vi wi] (Math/abs (- vi wi)) 
+        ) v w)
+    )
+)
+
+
+(println (de [1 0] [1 1]) )
+(println (de [0 0 0] [1 1 1]) )
+
+
+(println (dm [1 0] [1 1]) )
+(println (dm [0 0 0] [1 1 1])  )
