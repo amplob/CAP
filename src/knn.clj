1b2e6d620410d4259b83358c64ebf5f7e1bad4ad
giselabz <144422805+giselabz@users.noreply.github.com>
2024-10-15 17:29:56 +0200

    Funció classificació

--- src/knn.clj
+++ src/knn.clj
@@ -49,11 +49,14 @@
 
 
 (defn classificacio [taula ex fd]
-  (reduce min (map 
-    (fn [t1] (fd ex t1)) 
-    taula)
-  )
-
-)
+  (let [distances (map (fn [t1] 
+                          [(fd ex (:x t1)) (:y t1)]) 
+                        taula)]   
+    (reduce (fn [min current]
+              (if (< (first current) (first min))
+                current
+                min))
+            (first distances) 
+            distances)))
 
 (println  (classificacio iris tst-iris de) )
