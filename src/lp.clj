12682c2bbf97554dc397de35890bf7749b895f41
Amplob <amplob@gmail.com>
2025-01-12 19:39:30 +0100

    canvis perquè sigui més senzill i entendible

--- src/lp.clj
+++ src/lp.clj
@@ -4,78 +4,75 @@
 (ns lp
   (:require [clojure.string :as str]))
 
-(defn eval-exp [exp symbols]
-  (let [[op & args] exp]
-    (case op
-      :val (second exp)
-      :var (get symbols (second exp))
-      :add (+ (eval-exp (nth exp 1) symbols)
-              (eval-exp (nth exp 2) symbols))
-      :sub (- (eval-exp (nth exp 1) symbols)
-              (eval-exp (nth exp 2) symbols))
-      :mul (* (eval-exp (nth exp 1) symbols)
-              (eval-exp (nth exp 2) symbols))
-      :div (let [num (eval-exp (nth exp 1) symbols)
-                 den (eval-exp (nth exp 2) symbols)]
+; Funció per avaluar una expressió aritmètica donada una taula de símbols,
+(defn evalua-expressio [exp taula-simbols]
+  (let [[operacio & args] exp]
+    (case operacio
+      :val (second exp)  ; valor literal
+
+      :var (let [[_ var] exp] (taula-simbols var))  ; accedir a la variable a la taula
+
+      :add (let [[_ exp1 exp2] exp]
+             (+ (evalua-expressio exp1 taula-simbols)
+                (evalua-expressio exp2 taula-simbols)))
+
+      :sub (let [[_ exp1 exp2] exp]
+             (- (evalua-expressio exp1 taula-simbols)
+                (evalua-expressio exp2 taula-simbols)))
+
+      :mul (let [[_ exp1 exp2] exp]
+             (* (evalua-expressio exp1 taula-simbols)
+                (evalua-expressio exp2 taula-simbols)))
+
+      :div (let [[_ exp1 exp2] exp
+                 num (evalua-expressio exp1 taula-simbols)
+                 den (evalua-expressio exp2 taula-simbols)]
              (if (zero? den)
                (throw (ex-info "Divisió per zero!" {}))
-               (quot num den))))))  ; utilitzem integer division (quot) segons exemples
+               (int (/ num den)))))))
 
-(defn process-actions [actions symbols]
-  (if (empty? actions)
-    symbols
-    (let [action (first actions)
-          rest-actions (rest actions)
-          [op & args] action]
-      (case op
-        :ass (let [var-name (second action)
-                   expr (nth action 2)
-                   value (eval-exp expr symbols)
-                   new-symbols (assoc symbols var-name value)]
-               (process-actions rest-actions new-symbols))
-        :view (do
+;; Funció per processar una llista d'accions donada una taula de símbols inicial,
+(defn processa-accions [accions taula-simbols]
+  (if (empty? accions)
+    taula-simbols
+    (let [acció (first accions)
+          rest-accions (rest accions)
+          [operacio & args] acció]
+      (case operacio
+        :ass (let [[_ nom-var expr] acció
+                   valor (evalua-expressio expr taula-simbols)
+                   nova-taula (merge taula-simbols {nom-var valor})]
+               (processa-accions rest-accions nova-taula))
+        :view (let [[_ expr] acció]
                 (try 
-                  (println ">>" (eval-exp (second action) symbols))
+                  (println ">>" (evalua-expressio expr taula-simbols))
                   (catch Exception e
                     (println ">>" (.getMessage e))))
-                (process-actions rest-actions symbols))
-        ;; Si trobem una acció desconeguda, simplement la saltem
-        (process-actions rest-actions symbols)))))
+                (processa-accions rest-accions taula-simbols))
+        (processa-accions rest-accions taula-simbols)))))
 
 (defn interpretar [programa]
-  (process-actions programa {}))
+  (processa-accions programa {}))
 
-
-
-;;proves
+; Joc de proves
 (interpretar '((:ass "a" (:val 2))
-               (:view (:div (:var "a") (:val 0)))
-               (:view (:add (:var "a") (:val 1)))))
-
-(interpretar
- '((:ass "x" (:val 10))
-   (:ass "y" (:mul (:var "x") (:val 2)))
-   (:view (:add (:var "x") (:var "y")))))
+               (:view (:div (:var "a") (:val 0))) ; divisió per zero
+               (:view (:add (:var "a") (:val 1))))) ; 3
 
 (interpretar
  '((:ass "a" (:val 5))
    (:ass "b" (:val 3))
-   (:view (:sub (:var "a") (:var "b")))
-   (:view (:mul (:var "a") (:var "b")))))
+   (:view (:sub (:var "a") (:var "b"))) ; 2
+   (:view (:mul (:var "a") (:var "b"))))) ; 15
 
 (interpretar
- '((:ass "n" (:val 10))
-   (:ass "d" (:val 0))
-   (:view (:div (:var "n") (:var "d")))))
-
-(interpretar
- '((:view (:var "z"))
+ '((:view (:var "z")) ; nil
    (:ass "z" (:val 7))
-   (:view (:var "z"))))
+   (:view (:var "z")))) ; 7
 
 (interpretar
  '((:ass "x" (:val 4))
    (:ass "y" (:add (:var "x") (:val 6)))
    (:ass "z" (:mul (:var "y") (:val 2)))
-   (:view (:sub (:var "z") (:var "x"))) ;;20 - 4 = 16
-   (:view (:div (:var "z") (:var "x"))))) ;; 20 / 4 = 5
+   (:view (:sub (:var "z") (:var "x"))) ; 20 - 4 = 16
+   (:view (:div (:var "z") (:var "x"))))) ; 20 / 4 = 5
